af6571ee9dc9dde6cf3528f0e0f6ad314f2a3aef
Sean Matheson <sean@ctrlplusb.com>
2017-05-08 14:19:27 +0100

    Adds initial project layout

---
+++ packages/constellate-example-web-client/modules/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import ConstellateExampleReactApp from 'constellate-example-react-app'
+
+render(<ConstellateExampleReactApp />, document.getElementById('app-host'))
