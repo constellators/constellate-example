fa67ac4caa5caf2dc23eccb897f3c0837c8d65c3
Sean Matheson <sean@ctrlplusb.com>
2017-05-25 13:18:09 +0100

    More cleanup.

--- projects/myapp-web-client/constellate.js
+++ projects/myapp-web-client/constellate.js
@@ -1,9 +1,3 @@
 module.exports = {
   web: {},
-  plugins: {
-    // eslint-disable-next-line no-unused-vars
-    webpack: (defaultConfig, { project, webpack, development }) =>
-      // Augment or replace the default webpack config with your own.
-      defaultConfig,
-  },
 }
